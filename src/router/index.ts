c638ee43f5a7f3af1afb9722952ef43a4a5bbefc
Taichi <taichi@asaichi.co.jp>
2021-03-09 15:06:06 +0900

    土台作成

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import Login from "@/views/Login.vue";
+import Chat from "@/views/Chat.vue";
 
 Vue.use(VueRouter);
 
@@ -13,11 +16,17 @@ const routes: Array<RouteConfig> = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: About
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
+  },
+  {
+    path: "/chat",
+    name: "Chat",
+    component: Chat
   }
 ];
 
---
+++ src/views/Chat.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login">
+    Login
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {}
+})
+export default class Home extends Vue {}
+</script>
+<style scoped></style>
